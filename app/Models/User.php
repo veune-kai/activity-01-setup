aca6e0cefdd1bf2b524492d25f399e72cfe4890f
Karyl G <karylgilo@gmail.com>
2026-01-30 11:41:48 +0800

    Customize users table migration and model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',       
+        'is_active',  
     ];
 
     /**
@@ -40,5 +42,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'is_active' => 'boolean', 
     ];
 }
